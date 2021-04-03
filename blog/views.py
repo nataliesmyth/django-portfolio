73303fb32aaac4a8ad075c5baade35b0bffe8e14
Nataliesmyth <nataliesmyth11@gmail.com>
2021-04-03 08:34:16 -0400

    created blog detail view function

--- blog/views.py
+++ blog/views.py
@@ -13,3 +13,31 @@ def blog_index(request):
     return render(request, "blog_index.html", context)
 
 # create blog_category() view
+
+def blog_category(request, category):
+    # below we are using a Django Queryset filter
+    posts = Post.objects.filter(
+        categories__name__contains=category
+    ).order_by(
+        '-created_on'
+    )
+    context = {
+        "category": category,
+        "posts": posts
+    }
+    return render(request, "blog_category.html", context)
+
+    # The argument of the Django Queryset filter tells Django what conditions need to be met for an object to be retrieved.
+    # In this case, we only want posts whose categories contain the category with the name corresponding to that given in the argument of the view function., and using order_by() to order posts starting with the most recent
+    # Then we add these posts and the category to the context dictionary and render our template
+
+def blog_detail(request, pk):
+    # view function takes pk value as an argument and retrieves the object with the given pk
+    post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+    }
+
+    return render(request, "blog_detail.html", context)
