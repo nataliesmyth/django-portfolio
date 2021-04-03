3406b07c5dfa92263c0b64bb59eb244234bfaf34
Nataliesmyth <nataliesmyth11@gmail.com>
2021-04-03 08:01:50 -0400

    added notes

--- blog/models.py
+++ blog/models.py
@@ -12,8 +12,10 @@ class Post(models.Model):
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField('Category', related_name='posts')
 
-    class Comment(models.Model):
-        author = models.CharField(max_length=60)
-        body = models.TextField()
-        created_on = models.DateTimeField(auto_now_add=True)
-        post = models.ForeignKey('Post', on_delete=models.CASCADE)
+class Comment(models.Model):
+    author = models.CharField(max_length=60)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+
+    # We add the argument on_delete=models.CASCADE so that comments are deleted when posts are deleted
