7fabd42c31e87e39b04a2c624782e56fd2f3c759
Nataliesmyth <nataliesmyth11@gmail.com>
2021-04-03 06:27:57 -0400

    created Project class

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+# Textfield and Charfield are similar, but TextField can be used for longer form text because it doesn't have a max length limit.
+# FilePathField also holds a string, but must point to a file path name
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technology = models.CharField(max_length=20)
+    image = models.FilePathField(path="/img")
