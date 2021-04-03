72e176caceef93d7605de5b6e2bd67b6caa2b2c5
Nataliesmyth <nataliesmyth11@gmail.com>
2021-04-03 08:22:01 -0400

    blog index view created

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+# import Post and Comment models
+from blog.models import Post, Comment
 
 # Create your views here.
+# inside the view function (blog_index()), we obtain a Queryset containing all the posts in the database.order_by() orders the Queryset according to the agument given, with the minus sign telling Django to start with the largest value so the posts will be ordered witht he most recent one first
+
+def blog_index(request):
+    posts = Post.objects.all().order_by('-created_on')
+    context = {
+        "posts": posts,
+    }
+    return render(request, "blog_index.html", context)
+
+# create blog_category() view
