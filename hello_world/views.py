8ae3957f5502c5d25fff6adbf03d14165c5006d4
Nataliesmyth <nataliesmyth11@gmail.com>
2021-04-03 05:50:59 -0400

    added view functions

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Below we defined a VIEW FUNCTION called hello_world()
+# When the view fn is called, it will render an HTML file called hello_world.html
+# View fn takes one argument: request
+# This object is an HttpRequestObject that is created every time a page is loaded
+# HttpRequestObject contains info about the request,like the method that can take several values including GET and POST
+
+def hello_world(request):
+    # render() looks for HTML templates inside a directory called templates insode your app directory.
+    return render(request, 'hello_world.html', {})
